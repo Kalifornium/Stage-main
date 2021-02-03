8c196d46ec7e94e0c2e466fc0b0ecba0e7acf823
Kalifornium <dylan.hacquart91@gmail.com>
2021-02-03 14:20:00 +0100

    Enlevement du cercle de la maps "Contact"

--- contact.php
+++ contact.php
@@ -21,13 +21,13 @@
 
     <div style="height: 665px; line-height: 665px; border: 0px solid black; background: url('Images/fond_contact.png'); background-position: center center; background-repeat: no-repeat; background-size: cover;">
         <div style="border: 0px solid blue; text-align: left; width: 700px; margin: auto; vertical-align: middle; display: inline-block; line-height:1.2; width: 900px;">
-            <h2 style="font-size: 45px; color: white;">Contact</h2>
+            <h2 style="font-size: 45px; color: white; margin-top: 0px;">Contact</h2>
             <div style="display: flex; flex-direction: row;">
                 <div style="border: 0px solid green; background-color: white; width: 200px; height: 400px;">
                     <br><img src="Images/logo_contact.png" style="height: 200px;">
                     <br><strong>PRO TEK ISO</strong><br>
                     <i>85 Rue Gallieni<br>
-                    75000 Paris</i><br>
+                    95170 Deuil-la-Barre</i><br>
                     <br>
                     +33 6 43 66 65 86<br>
                     <br>
@@ -47,8 +47,9 @@
     </div>
 
     <!-- Maps de Google Maps pour délimiter le périmètre du champ d'action -->
-    <div style="height: 37.500em;">
-        <iframe src="https://www.google.com/maps/d/embed?mid=19Sh4SGqceS3Fpzu6ZBtziD7KiOQb1R89"></iframe>
+    <div style="height: 37.500em;" class="map-responsive">
+        <!-- <iframe src="https://www.google.com/maps/d/embed?mid=19Sh4SGqceS3Fpzu6ZBtziD7KiOQb1R89"></iframe> -->
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2619.2148886800073!2d2.3273660222162493!3d48.9684336976623!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e668fcd90c2d27%3A0xe4fba7e345ccb738!2s85%20Rue%20Galli%C3%A9ni%2C%2095170%20Deuil-la-Barre!5e0!3m2!1sfr!2sfr!4v1612357561626!5m2!1sfr!2sfr" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
     </div>
 
     <div style="height: 5.500em; background-color: #242323; border: 1px solid #242323;" id="contenu">
